0798d4d0969debf36c95fc3be56ca19c5a5ba5d5
Daniel Chuang <dc863@cornell.edu>
2024-11-26 00:02:25 -0500

    Push w/ some UI refactoring

--- Pods/Target Support Files/Pods-score-ios/Pods-score-ios-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_score_ios : NSObject
-@end
-@implementation PodsDummy_Pods_score_ios
-@end
--- score-ios/Views/FilterTile.swift
+++ score-ios/Views/FilterTile.swift
@@ -1,105 +1,39 @@
 //
-//  FilterTile.swift
-//  score-ios
-//
 //  Created by Daniel Chuang on 9/15/24.
 //
 
 import SwiftUI
-import SwiftSVG
 
-// MARK: - Layout
-private struct TileLayout {
-    static let iconSize: CGFloat = 32
-    
-    struct Padding {
-        static let locationTextPadding: CGFloat = 6
-    }
-}
-
-private typealias TL = TileLayout
-
-// MARK: - SVG Render
-struct SVGShape: Shape {
-    let svg: SVG
-
-    func path(in rect: CGRect) -> Path {
-        let transformedPath = svg.path.cgPath(byTransforming: CGAffineTransform.identity.scaledBy(x: rect.width, y: rect.height).translatedBy(x: rect.minX, y: rect.minY))
-        return Path(transformedPath)
-    }
-}
-
-// MARK: - FilterTile UI
 struct FilterTile : View {
     
     var sport: Sport
     var selected: Bool
     
     var body : some View {
-//        let imageName: String = selected ? sport.rawValue+"-r": sport.rawValue+"-g"
-        let imageName : String = sport.rawValue+"-g"
-        let color: Color = selected ? Constants.Colors.primary_red : Constants.Colors.iconGray
-
-        
-        VStack(spacing: TL.Padding.locationTextPadding) {
-            // Icon
+        VStack(spacing: 6) {
             
-            Image(imageName)
-                .resizable()
-                .renderingMode(.template)
-                .frame(width: TL.iconSize, height: TL.iconSize)
-                .blendMode(.destinationOut)
-            
-            ZStack {
-                Image(imageName)
+            if (selected) {
+                Image(sport.rawValue+"-r")
                     .resizable()
                     .renderingMode(.template)
-                    .frame(width: TL.iconSize, height: TL.iconSize)
-                    .foregroundStyle(color)
-
-                if selected {
-                    Rectangle()
-                        .fill(.clear)
-                        .frame(width: TL.iconSize, height: TL.iconSize)
-                        .reverseMask{
-                            Image(imageName)
-                                .resizable()
-                                .renderingMode(.template)
-                                .frame(width: TL.iconSize, height: TL.iconSize)
-                                .foregroundStyle(color)
-                        }
-//                        .clipShape()
-                }
-            }.compositingGroup()
+                    .frame(width: 32, height: 32)
+                    .foregroundStyle(Constants.Colors.selected)
+            } else {
+                Image(sport.rawValue+"-g")
+                    .resizable()
+                    .renderingMode(.template)
+                    .frame(width: 32, height: 32)
+                    .foregroundStyle(Constants.Colors.iconGray)
+            }
             
-
-            
-            // Text
             Text(sport.description)
                 .foregroundStyle(selected ? Constants.Colors.selected : Constants.Colors.iconGray)
                 .font(Constants.Fonts.sportLabel)
-                .foregroundStyle(color)
         }
+        .frame(minWidth: 56)
     }
 }
 
 #Preview () {
-    FilterTile(sport: .Basketball, selected: true)
-}
-
-// https://stackoverflow.com/questions/73755862/swiftui-transparent-part-of-the-view-and-text
-public extension View {
-    @inlinable
-    func reverseMask<Mask: View>(
-        alignment: Alignment = .center,
-        @ViewBuilder _ mask: () -> Mask
-    ) -> some View {
-        self.mask {
-            Rectangle()
-                .overlay(alignment: alignment) {
-                    mask()
-                        .blendMode(.destinationOut)
-                }
-        }
-    }
+    FilterTile(sport: .Soccer, selected: false)
 }

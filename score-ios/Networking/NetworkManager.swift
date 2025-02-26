deb5521749ca53509e291762066827debf648981
Daniel Chuang <dc863@cornell.edu>
2025-02-26 18:26:00 -0500

    Resolve PR changes

--- score-ios/Networking/NetworkManager.swift
+++ score-ios/Networking/NetworkManager.swift
@@ -29,26 +29,6 @@ class NetworkManager {
         }
     }
     
-//    func filterUpcomingGames(gender: String?, sport: String?, completion: @escaping ([GamesQuery.Data.Game]?, Error?) -> Void) {
-//        apolloClient.fetch(query: GamesQuery()) { result in
-//            switch result {
-//            case .success(let graphQLResult):
-//                if let gamesData = graphQLResult.data?.games?.compactMap({ $0 }) {
-//                    let filteredGames = gamesData.filter { game in
-//                        (gender == nil || game.gender == gender) &&
-//                        (sport == nil || game.sport == sport)
-//                    }
-//                    completion(filteredGames, nil)
-//                } else if let errors = graphQLResult.errors {
-//                    let errorDescription = errors.map { $0.localizedDescription }.joined(separator: "\n")
-//                    completion(nil, NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: errorDescription]))
-//                }
-//            case .failure(let error):
-//                completion(nil, error)
-//            }
-//        }
-//    }
-    
     func fetchTeamById(by id: String, completion: @escaping (GetTeamByIdQuery.Data.Team?, Error?) -> Void) {
         let query = GetTeamByIdQuery(id: id)
         

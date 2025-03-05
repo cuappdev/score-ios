c76799a9b42f72c33eae542289b32af301329114
AdelynnWu <150749410+AdelynnWu@users.noreply.github.com>
2025-03-05 18:16:05 -0500

    fixed secrets

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#  ci_post_clone.sh
+#  score-ios
+#
+#  Created by Hsia Lu wu on 3/5/25.
+#  
+
+echo "Downloading Secrets"
+brew install wget
+cd SCI_PRIMARY_REPOSITORY_PATH/ci_scripts
+mkdir ../ScoreSecrets
+wget -O ../score_ios/ScoreSecrets/Keys.xcconfig "$KEYS"
+wget -O ../score_ios/ScoreSecrets/apollo-codegen-config-dev.json "$CODEGEN_DEV"
+wget -O ../score_ios/ScoreSecrets/apollo-codegen-config-prod.json "$CODEGEN_PROD"

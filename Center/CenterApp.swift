1ef2b54be5f840217694642e927f4dd888c84a90
Peter <mrpeterssyt@gmail.com>
2025-09-10 17:39:57 -0400

    Initial Commit

---
+++ Center/CenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CenterApp.swift
+//  Center
+//
+//  Created by Peter Bidoshi on 9/10/25.
+//
+
+import SwiftUI
+
+@main
+struct CenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

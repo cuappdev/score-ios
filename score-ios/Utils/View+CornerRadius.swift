c70fcd598e928478b9b01312d75db1d94fe434c7
Zain Bilal <zainbilalgit@gmail.com>
2025-10-03 14:21:17 -0400

    added util to add corner radius on specified corners

---
+++ score-ios/Utils/View+CornerRadius.swift
@@ -0,0 +1,28 @@
+//
+//  View+CornerRadius.swift
+//  score-ios
+//
+//  Created by Zain Bilal on 10/3/25.
+//
+
+import SwiftUI
+
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(
+            roundedRect: rect,
+            byRoundingCorners: corners,
+            cornerRadii: CGSize(width: radius, height: radius)
+        )
+        return Path(path.cgPath)
+    }
+}
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape(RoundedCorner(radius: radius, corners: corners))
+    }
+}
